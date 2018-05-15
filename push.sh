435214ee92d9604a2bfa96560141d97febef7b1b
BFunk <bret.e.funk@gmail.com>
2018-05-15 13:53:07 -0500

    Updated on Tue May 15 13:53:07 -05 2018

--- push.sh
+++
@@ -1,3 +0,0 @@
-git add .
-git commit -m "Updated on $(date)"
-git push
--- scripts.sh
+++ scripts.sh
@@ -11,5 +11,5 @@ ln -s ~/dotfiles/zshrc ~/.zshrc
 ln -s ~/dotfiles/gitignore ~/.gitignore
 
 git add .
-git commit -m "Updated Dotfiles on $(date)"
+git commit -m "Updated on $(date)"
 git push
