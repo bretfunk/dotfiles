f890c789607828fabc6508e0e4466237c51c3c92
Bret Funk <bret.e.funk@gmail.com>
2020-02-03 19:13:48 -0500

    Updated on Mon Feb  3 19:13:48 -05 2020

--- scripts.sh
+++ scripts.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-rm ~/.vim/vimrc
+rm ~/.config/nvim/init.vim
 rm ~/.bashrc
 rm ~/.zshrc
 rm ~/.gitignore
 
-ln -s ~/dotfiles/vimrc ~/.vim/vimrc
+ln -s ~/dotfiles/vimrc ~/config/nvim/init.vim
 ln -s ~/dotfiles/bashrc ~/.bashrc
 ln -s ~/dotfiles/zshrc ~/.zshrc
 ln -s ~/dotfiles/gitignore ~/.gitignore
