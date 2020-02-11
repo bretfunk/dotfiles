ffe6829b275b475ce1b98cf76b24105e92e48e1e
Bret Funk <bret.e.funk@gmail.com>
2020-02-10 21:30:27 -0500

    Updated on Mon Feb 10 21:30:27 -05 2020

--- scripts.sh
+++ scripts.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 rm ~/.config/nvim/init.vim
+rm ~/.config/nvim/coc-settings.json
 rm ~/.bashrc
 rm ~/.zshrc
 rm ~/.gitignore
 
 ln -s ~/dotfiles/vimrc ~/.config/nvim/init.vim
+ln -s ~/dotfiles/coc-settings ~/.config/nvim/coc-settings.json
 ln -s ~/dotfiles/bashrc ~/.bashrc
 ln -s ~/dotfiles/zshrc ~/.zshrc
 ln -s ~/dotfiles/gitignore ~/.gitignore
