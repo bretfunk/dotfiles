45fab0e325438df6e5fd4d5e902a8c8595eaac36
Bret Funk <bret.e.funk@gmail.com>
2020-04-09 09:18:18 -0500

    Updated on Thu Apr  9 09:18:18 CDT 2020

--- scripts.sh
+++ scripts.sh
@@ -7,12 +7,12 @@
 # rm ~/.zshrc
 # rm ~/.gitignore
 
-ln -s ~/dotfiles/vimrc ~/.config/nvim/init.vim
-ln -s ~/dotfiles/coc-settings ~/.config/nvim/coc-settings.json
-ln -s ~/dotfiles/kitty ~/.config/kitty/kitty.conf
-ln -s ~/dotfiles/bashrc ~/.bashrc
-ln -s ~/dotfiles/zshrc ~/.zshrc
-ln -s ~/dotfiles/gitignore ~/.gitignore
+ln -s -f ~/dotfiles/vimrc ~/.config/nvim/init.vim
+ln -s -f ~/dotfiles/coc-settings ~/.config/nvim/coc-settings.json
+ln -s -f ~/dotfiles/kitty ~/.config/kitty/kitty.conf
+ln -s -f ~/dotfiles/bashrc ~/.bashrc
+ln -s -f ~/dotfiles/zshrc ~/.zshrc
+ln -s -f ~/dotfiles/gitignore ~/.gitignore
 
 # only push to github if main computer
 COMP=$(scutil --get ComputerName)
