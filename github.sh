b92bbae3656fdeb1a6fe67906ec79f620beac3d2
Bret Funk <bret.e.funk@gmail.com>
2022-04-24 10:23:27 -0400

    test

--- github.sh
+++ github.sh
@@ -6,9 +6,8 @@ a=0
 
 while [ $a -lt 5 ]
 do
-   echo $a
-   a=`expr $a + 1`
-
    echo "test" >> ~/notes/github_garden.txt
    git add . && git commit -m "test" && git push
+   a=`expr $a + 1`
+
 done
